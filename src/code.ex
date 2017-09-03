551fc49b216db3094472451678b6f16845ce6505
Matúš Kmiť <matus.kmit@zhdk.ch>
2017-09-03 21:06:22 +0200

    finish

--- src/code.ex
+++ src/code.ex
@@ -1,36 +1,67 @@
-defmodule MyLogger do
-  def start_link do
-    Task.start_link(fn -> loop("") end)
-  end
+# defmodule MyLogger do
+#   def start_link do
+#     Task.start_link(fn -> loop("") end)
+#   end
 
-  defp loop(log) do
-    receive do
-      {:get, caller} ->
-        send caller, log
-        loop(log)
-      {:put, value} ->
-        loop(log <> value)
-    end
-  end
-end
+#   defp loop(log) do
+#     receive do
+#       {:get, caller} ->
+#         send caller, log
+#         loop(log)
+#       {:put, value} ->
+#         loop(log <> value)
+#     end
+#   end
+# end
+
+# defmodule MyMath do
+#   def init_log do
+#     {:ok, pid} = MyLogger.start_link
+#     Process.register(pid, :log)
+#   end
+
+#   def add1(x) do
+#     send(:log, {:put, "add1"})
+#     x + 1
+#   end
+
+#   def square(x) do
+#     send(:log, {:put, "square"})
+#     x * x
+#   end
+
+#   def log do
+#     send(:log, {:get, self()})
+#   end
+# end
+
+# import MyMath
+# MyMath.init_log # => true
+# square(add1 1) # => 4
+# log # => {:get, #PID<0.91.0>}
+# flush # => "add1square", :ok
 
 defmodule MyMath do
   def init_log do
-    {:ok, pid} = MyLogger.start_link
-    Process.register(pid, :log)
+    Agent.start_link(fn -> "" end, name: :log)
   end
 
   def add1(x) do
-    send(:log, {:put, "add1"})
+    Agent.update(:log, fn log -> log <> "add1" end)
     x + 1
   end
 
   def square(x) do
-    send(:log, {:put, "square"})
+    Agent.update(:log, fn log -> log <> "square" end)
     x * x
   end
 
   def log do
-    send(:log, {:get, self()})
+    Agent.get(:log, fn log -> log end)
   end
 end
+
+# import MyMath
+# init_log # => {:ok, #PID<0.91.0>}
+# square(add1 1) # => 4
+# log # => "add1square"
